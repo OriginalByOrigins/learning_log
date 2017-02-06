88dcb72c0f7fa5028ef68fa1555c027c535d4afd
se7en <se7en@example.com>
2017-02-06 14:39:04 +0800

    Added custom 404 and 500 error pages.

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '^gf*)72(q9^6rnl03#@_rky6h^mc7h)f&)p$@qnrj2m1dvx4_t'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = []
 
 
 # Application definition
