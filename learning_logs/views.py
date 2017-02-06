a8700342ea52f8732ba3ab223e786e990ccd9c73
se7en <se7en@example.com>
2017-02-06 14:47:54 +0800

    Raise 404 when request non-existing date

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Topic, Entry
 
 from django.http import HttpResponseRedirect, Http404
@@ -23,7 +23,7 @@ def topics(request):
 @login_required
 def topic(request, topic_id):
     """Show a single topic and all its entries."""
-    topic = Topic.objects.get(id=topic_id)
+    topic = get_object_or_404(Topic, topic_id)
     # Make sure the topic belong to the current user.
     check_topic_owner(request, topic)
 
